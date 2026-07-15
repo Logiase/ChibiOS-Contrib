92af2fde9b10373546ff4ccba4e9a6838c45b38c
Logiase Song <logiase.song@gmail.com>
2026-07-15 18:03:28 +0800

    Add test demo.

--- demos/MCX/RT-FRDMMCXN947/cfg/mcuconf.h
+++ demos/MCX/RT-FRDMMCXN947/cfg/mcuconf.h
@@ -5,4 +5,15 @@
 
 #define MCX_ST_IRQ_PRIORITY                 7
 
+#define MCX_SERIAL_USE_LPUART0              FALSE
+#define MCX_SERIAL_USE_LPUART1              FALSE
+#define MCX_SERIAL_USE_LPUART2              FALSE
+#define MCX_SERIAL_USE_LPUART3              FALSE
+#define MCX_SERIAL_USE_LPUART4              TRUE
+#define MCX_SERIAL_USE_LPUART5              FALSE
+#define MCX_SERIAL_USE_LPUART6              FALSE
+#define MCX_SERIAL_USE_LPUART7              FALSE
+#define MCX_SERIAL_USE_LPUART8              FALSE
+#define MCX_SERIAL_USE_LPUART9              FALSE
+
 #endif /* MCUCONF_H */
--- demos/MCX/RT-FRDMMCXN947/main.c
+++ demos/MCX/RT-FRDMMCXN947/main.c
@@ -1,53 +1,72 @@
 #include "ch.h"
 #include "hal.h"
 
-static volatile uint32_t heartbeat;
+#if defined (__TEST_RT)
+#include "rt_test_root.h"
+#endif
+#if defined (__TEST_OSLIB)
+#include "oslib_test_root.h"
+#endif
 
-typedef struct {
-  ioline_t line;
-  bool led_on;
-} ButtonWaitConfig;
+/* Testing in progress.*/
+static bool testing = false;
 
-static ButtonWaitConfig sw2_wait = {
-  .line = LINE_BUTTON_SW2,
-  .led_on = true,
-};
+/*
+ * Green LED blinker thread, times are in milliseconds.
+ */
+static THD_WORKING_AREA(waThread1, 256);
+static THD_FUNCTION(Thread1, arg) {
 
-static ButtonWaitConfig sw3_wait = {
-  .line = LINE_BUTTON_SW3,
-  .led_on = false,
-};
-
-static THD_WORKING_AREA(waSw2Thread, 128);
-static THD_WORKING_AREA(waSw3Thread, 128);
-
-static THD_FUNCTION(ButtonWaitThread, arg) {
-  const ButtonWaitConfig *config = (const ButtonWaitConfig *)arg;
-
+  (void)arg;
+  chRegSetThreadName("blinker");
   while (true) {
-    if (palWaitLineTimeout(config->line, TIME_INFINITE) == MSG_OK) {
-      if (config->led_on) {
-        palClearLine(LINE_LED_RED);
-      } else {
-        palSetLine(LINE_LED_RED);
-      }
-    }
+    palToggleLine(LINE_LED_GREEN);
+    chThdSleepMilliseconds(testing ? 100 : 500);
   }
 }
 
+/*
+ * Application entry point.
+ */
 int main(void) {
 
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
   halInit();
   chSysInit();
 
-  palEnableLineEvent(LINE_BUTTON_SW2, PAL_EVENT_MODE_RISING_EDGE);
-  palEnableLineEvent(LINE_BUTTON_SW3, PAL_EVENT_MODE_FALLING_EDGE);
+  /*
+   * Activates the Serial or SIO driver using the default configuration.
+   */
+  sdStart(&SD4, NULL);
 
-  chThdCreateStatic(waSw2Thread, sizeof(waSw2Thread), NORMALPRIO, ButtonWaitThread, (void *)&sw2_wait);
-  chThdCreateStatic(waSw3Thread, sizeof(waSw3Thread), NORMALPRIO, ButtonWaitThread, (void *)&sw3_wait);
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 10, Thread1, NULL);
 
+  /*
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and check the button state.
+   */
   while (true) {
-    heartbeat++;
+#if defined (__TEST_RT) || defined (__TEST_OSLIB)
+    if (palReadLine(LINE_BUTTON_SW2) == PAL_LOW) {
+      testing = true;
+#if defined (__TEST_RT)
+      test_execute((BaseSequentialStream *)&SD4, &rt_test_suite);
+#endif
+#if defined (__TEST_OSLIB)
+      test_execute((BaseSequentialStream *)&SD4, &oslib_test_suite);
+#endif
+      testing = false;
+    }
+#endif
     chThdSleepMilliseconds(500);
   }
 }
