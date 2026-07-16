0b91abd6fa12e0f308423ed1a0807725d6c66879
Logiase Song <logiase.song@nxp.com>
2026-07-16 10:41:38 +0800

    Update chconf.h to pass the test.

--- demos/MCX/RT-FRDMMCXN947/cfg/chconf.h
+++ demos/MCX/RT-FRDMMCXN947/cfg/chconf.h
@@ -563,7 +563,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_SYSTEM_STATE_CHECK)
-#define CH_DBG_SYSTEM_STATE_CHECK           TRUE
+#define CH_DBG_SYSTEM_STATE_CHECK           FALSE
 #endif
 
 /**
@@ -574,7 +574,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_ENABLE_CHECKS)
-#define CH_DBG_ENABLE_CHECKS                TRUE
+#define CH_DBG_ENABLE_CHECKS                FALSE
 #endif
 
 /**
@@ -586,7 +586,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_ENABLE_ASSERTS)
-#define CH_DBG_ENABLE_ASSERTS               TRUE
+#define CH_DBG_ENABLE_ASSERTS               FALSE
 #endif
 
 /**
@@ -596,7 +596,7 @@
  * @note    The default is @p CH_DBG_TRACE_MASK_DISABLED.
  */
 #if !defined(CH_DBG_TRACE_MASK)
-#define CH_DBG_TRACE_MASK                   CH_DBG_TRACE_MASK_ALL
+#define CH_DBG_TRACE_MASK                   CH_DBG_TRACE_MASK_DISABLED
 #endif
 
 /**
@@ -619,7 +619,7 @@
  *          @p panic_msg variable set to @p NULL.
  */
 #if !defined(CH_DBG_ENABLE_STACK_CHECK)
-#define CH_DBG_ENABLE_STACK_CHECK           TRUE
+#define CH_DBG_ENABLE_STACK_CHECK           FALSE
 #endif
 
 /**
@@ -631,7 +631,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_FILL_THREADS)
-#define CH_DBG_FILL_THREADS                 TRUE
+#define CH_DBG_FILL_THREADS                 FALSE
 #endif
 
 /**
@@ -815,87 +815,3 @@
 #endif  /* CHCONF_H */
 
 /** @} */
-
-
-// #ifndef CHCONF_H
-// #define CHCONF_H
-
-// #define _CHIBIOS_RT_CONF_
-// #define _CHIBIOS_RT_CONF_VER_7_0_
-
-// #define CH_CFG_SMP_MODE                     FALSE
-
-// #define CH_CFG_ST_RESOLUTION                32
-// #define CH_CFG_ST_FREQUENCY                 1000
-// #define CH_CFG_INTERVALS_SIZE               32
-// #define CH_CFG_TIME_TYPES_SIZE              32
-// #define CH_CFG_ST_TIMEDELTA                 0
-
-// #define CH_CFG_TIME_QUANTUM                 0
-// #define CH_CFG_MEMCORE_SIZE                 0
-// #define CH_CFG_NO_IDLE_THREAD               FALSE
-// #define CH_CFG_OPTIMIZE_SPEED               TRUE
-
-// #define CH_CFG_USE_TM                       FALSE
-// #define CH_CFG_USE_TIMESTAMP                FALSE
-// #define CH_CFG_USE_REGISTRY                 FALSE
-// #define CH_CFG_USE_WAITEXIT                 FALSE
-// #define CH_CFG_USE_SEMAPHORES               FALSE
-// #define CH_CFG_USE_SEMAPHORES_PRIORITY      FALSE
-// #define CH_CFG_USE_MUTEXES                  FALSE
-// #define CH_CFG_USE_MUTEXES_RECURSIVE        FALSE
-// #define CH_CFG_USE_CONDVARS                 FALSE
-// #define CH_CFG_USE_CONDVARS_TIMEOUT         FALSE
-// #define CH_CFG_USE_EVENTS                   FALSE
-// #define CH_CFG_USE_EVENTS_TIMEOUT           FALSE
-// #define CH_CFG_USE_MESSAGES                 FALSE
-// #define CH_CFG_USE_MESSAGES_PRIORITY        FALSE
-// #define CH_CFG_USE_DYNAMIC                  FALSE
-
-// #define CH_CFG_USE_MAILBOXES                FALSE
-// #define CH_CFG_USE_MEMCORE                  FALSE
-// #define CH_CFG_USE_HEAP                     FALSE
-// #define CH_CFG_USE_MEMPOOLS                 FALSE
-// #define CH_CFG_USE_OBJ_FIFOS                FALSE
-// #define CH_CFG_USE_PIPES                    FALSE
-// #define CH_CFG_USE_OBJ_CACHES               FALSE
-// #define CH_CFG_USE_DELEGATES                FALSE
-// #define CH_CFG_USE_JOBS                     FALSE
-// #define CH_CFG_USE_FACTORY                  FALSE
-// #define CH_CFG_FACTORY_MAX_NAMES_LENGTH     0
-// #define CH_CFG_FACTORY_OBJECTS_REGISTRY     FALSE
-// #define CH_CFG_FACTORY_GENERIC_BUFFERS      FALSE
-// #define CH_CFG_FACTORY_SEMAPHORES           FALSE
-// #define CH_CFG_FACTORY_MAILBOXES            FALSE
-// #define CH_CFG_FACTORY_OBJ_FIFOS            FALSE
-// #define CH_CFG_FACTORY_PIPES                FALSE
-
-// #define CH_DBG_STATISTICS                   FALSE
-// #define CH_DBG_SYSTEM_STATE_CHECK           FALSE
-// #define CH_DBG_ENABLE_CHECKS                FALSE
-// #define CH_DBG_ENABLE_ASSERTS               FALSE
-// #define CH_DBG_TRACE_MASK                   CH_DBG_TRACE_MASK_DISABLED
-// #define CH_DBG_TRACE_BUFFER_SIZE            0
-// #define CH_DBG_ENABLE_STACK_CHECK           FALSE
-// #define CH_DBG_FILL_THREADS                 FALSE
-// #define CH_DBG_THREADS_PROFILING            FALSE
-
-// #define CH_CFG_SYSTEM_EXTRA_FIELDS
-// #define CH_CFG_SYSTEM_INIT_HOOK()
-// #define CH_CFG_OS_INSTANCE_EXTRA_FIELDS
-// #define CH_CFG_OS_INSTANCE_INIT_HOOK(oip)
-// #define CH_CFG_THREAD_EXTRA_FIELDS
-// #define CH_CFG_THREAD_INIT_HOOK(tp)
-// #define CH_CFG_THREAD_EXIT_HOOK(tp)
-// #define CH_CFG_CONTEXT_SWITCH_HOOK(ntp, otp)
-// #define CH_CFG_IRQ_PROLOGUE_HOOK()
-// #define CH_CFG_IRQ_EPILOGUE_HOOK()
-// #define CH_CFG_IDLE_ENTER_HOOK()
-// #define CH_CFG_IDLE_LEAVE_HOOK()
-// #define CH_CFG_IDLE_LOOP_HOOK()
-// #define CH_CFG_SYSTEM_TICK_HOOK()
-// #define CH_CFG_SYSTEM_HALT_HOOK(reason)
-// #define CH_CFG_TRACE_HOOK(tep)
-// #define CH_CFG_RUNTIME_FAULTS_HOOK(mask)
-
-// #endif /* CHCONF_H */
